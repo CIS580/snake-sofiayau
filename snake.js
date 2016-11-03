2cc5ff882f33f1b82366fa4b9a4749d9569531a6
Sofia <arcsofia.yau@hotmail.com>
2016-11-03 15:20:12 -0500

    latest modified

--- snake.js
+++ snake.js
@@ -2,29 +2,29 @@
 var frontBuffer = document.getElementById('snake');
 var frontCtx = frontBuffer.getContext('2d');
 var backBuffer = document.createElement('canvas');
-var speed = 1/12/1000;
+var speed = 3;
 var score = 0;
+var gameOver = false;
+var appleEaten = false;
+var snail = new Image();
+snail.src = 'snail.svg';
+
+console.log(snail.x, snail.y);
 backBuffer.width = frontBuffer.width;
 backBuffer.height = frontBuffer.height;
 var backCtx = backBuffer.getContext('2d');
-var apple = {x: -1, y: 0};
-var snakeSize = 10;
+var snakeSize = 20;
 var oldTime = performance.now();
-var x = 0;
+var gameOver = false;
+
 var snake = [];
-var length = 5;
-  //push 3 elements of array, x=0, y=index
-for(var i =length; i >= 0; i--){
+var length = 3;
+var food = {x: Math.floor(Math.random()*30),
+        y:Math.floor(Math.random()*20)};
+for(var i = length; i >= 0; i--){
   snake.push({x:i, y:0});
-}
-
-/*function init(){
-drawSnake();
-spawnApple();
-setInterval(loop,80);
-}*/
-
-
+ }
+  //push 3 elements of array, x=0, y=index
 var input = {
   up:false,
   down:false,
@@ -37,19 +37,17 @@ var input = {
  * @param{time} the current time as a DOMHighResTimeStamp
  */
 function loop(newTime) {
-  var elapsedTime = newTime - oldTime;
-  oldTime = newTime;
-
-  update(elapsedTime);
-  render(elapsedTime);
-
-  // Flip the back buffer
-  frontCtx.drawImage(backBuffer, 0, 0);
-  // Run the next loop
-  window.requestAnimationFrame(loop);
+  if(gameOver == false){
+    var elapsedTime = newTime - oldTime;
+    oldTime = newTime;
+    update(elapsedTime);
+    render(elapsedTime);
+    // Flip the back buffer
+    frontCtx.drawImage(backBuffer, 0, 0);
+    // Run the next loop
+    window.requestAnimationFrame(loop);
+  }
 }
-
-
 /**
  * @function update
  * Updates the game state, moving
@@ -58,7 +56,6 @@ function loop(newTime) {
  * @param {elapsedTime} A DOMHighResTimeStamp indicting
  * the number of milliseconds passed since the last frame.
  */
-
  window.onkeydown = function(event){
   event.preventDefault();
    switch(event.keyCode){
@@ -66,7 +63,6 @@ function loop(newTime) {
    case 38:
    case 87:
      input.up = true;
-
      break;
      //left
    case 37:
@@ -93,7 +89,6 @@ function loop(newTime) {
    case 38:
    case 87:
      input.up = false;
-
      break;
      //left
    case 37:
@@ -113,15 +108,8 @@ function loop(newTime) {
    }
    return true;
  }
- function gameOver(){
-   //the warning of game over
-   frontCtx.fillRect(0,0,frontBuffer.width,frontBuffer.height);
-   frontCtx.fillStyle = "red";
-   frontCtx.fillText("game over",frontBuffer.width,frontBuffer.height);
-   //frontCtx.fillText("score"+scoreText()+frontBuffer.width,frontBuffer.height);
- }
-
 function update(elapsedTime) {
+
   //draw the path of the snake
   var snakeX = snake[0].x;
   var snakeY = snake[0].y;
@@ -130,25 +118,22 @@ function update(elapsedTime) {
     if(input.down) snakeY += 1;
     if(input.left)  snakeX -= 1;
     if(input.right)  snakeX += 1;
-
-
   // TODO: Spawn an apple periodically
   // TODO: Grow the snake periodically
   // TODO: Move the snake
   //spawn apples randomly
   var spawnApple = function(){
-    apple = {
+    food = {
       x: Math.floor((Math.random() * 30)+1),
-      y: Math.floor((Math.random() * 50)+1)
+      y: Math.floor((Math.random() * 20)+1)
     }
     //position of the snake
     for (var i=0; i<snake.lenth; i++){
       var snakeX = snake[i].x;
       var snakeY = snake[i].y;
-
-      if(apple.x === snakeX ||apple.y === snakeY && apple.x === snakeX){
-        apple.x = Math.floor((Math.random() * 30)+1);
-        apple.y = Math.floor((Math.random() * 30)+1);
+      if(food.x === snakeX && food.y === snakeY){
+        food.x = Math.floor((Math.random() * 30)+1);
+        food.y = Math.floor((Math.random() * 20)+1);
       }
     }
   }
@@ -162,22 +147,26 @@ function update(elapsedTime) {
     }
     return false;
   }
-
-
   // TODO: Determine if the snake has moved out-of-bounds (offscreen)
-  if(snakeX == -1 || snakeX == backBuffer.width / snakeSize || snakeY == -1 ||snakeY == backBuffer.height/snakeSize || checkCollision(snakeX,snakeY,snake)){
-    backCtx.clearRect(0,0,backBuffer.width, backBuffer.height);
-    gameOver();
-    return;
+  if(snakeX <0 || snakeX > backBuffer.width / snakeSize || snakeY < 0 ||snakeY > backBuffer.height/snakeSize || checkCollision(snakeX,snakeY,snake)){
+    console.log(snakeX,snakeY);
+    //document.getElementById("snake").innerHTML = "changed!";
+    if(snakeX == -1 || snakeX == 760/20 || snakeY == -1 || snakeY == 480/20) {
+      gameOver = true;
+      console.log('go');
+    }
+ return;
   }
   // TODO: Determine if the snake has eaten an apple
   // TODO: Determine if the snake has eaten its tail
-  if (snakeX == apple.x && snakeY == apple.y){
+  if (snakeX == food.x && snakeY == food.y){
     var tail = {
       x:snakeX,
       y:snakeY
     };
-    score++;
+    score += 10;
+    appleEaten = true;
+    snake[snake.length] = {x: snake[snake.length-1].x, y: snake[snake.length-1].y};
     spawnApple();
   }else{
     var tail = snake.pop();
@@ -185,10 +174,12 @@ function update(elapsedTime) {
     tail.y = snakeY;
     snake.unshift(tail);
   }
+  if(snakeX == 220 && snakeY == 200){
+    console.log(snail.x, snail.y);
+    gameOver = true;
+  }
   // TODO: [Extra Credit] Determine if the snake has run into an obstacle
-
 }
-
 /**
   * @function render
   * Renders the current game state into a back buffer.
@@ -197,36 +188,40 @@ function update(elapsedTime) {
   */
 function render(elapsedTime) {
   // TODO: Draw the game objects into the backBuffer
-  backCtx.fillStyle = "grey";
+  backCtx.fillStyle = "lightblue";
   backCtx.fillRect(0,0,backBuffer.width,backBuffer.height);
   var snakeBody = function(x,y){
-    backCtx.fillStyle = "green";
+    backCtx.fillStyle = "lightgreen";
     backCtx.fillRect(x * snakeSize, y * snakeSize, snakeSize, snakeSize);
-    console.log(x * snakeSize, y * snakeSize, snakeSize, snakeSize);
+    //console.log(x * snakeSize, y * snakeSize, snakeSize, snakeSize);
    }
-   var food = function(x,y){
+   var apple = function(x,y){
      backCtx.fillStyle = "red";
      backCtx.fillRect(x * snakeSize, y * snakeSize, snakeSize, snakeSize);
    }
    var scoreText = function(){
-     var score_text = "score" + score;
+     var score_text = "score: " + score;
      backCtx.fillStyle = "blue";
-     backCtx.fillText(score_text,145,20);//counting scores
+     backCtx.fillText(score_text, 20, 20);//counting scores
    }
-
    //body of the snake
-
-
    for(var i =0; i<snake.length; i++){
      snakeBody(snake[i].x,snake[i].y);
+
    }
-   snakeBody(20, 20);
-   food(apple.x, apple.y);
+   console.log(food.x, food.y);
+   backCtx.drawImage(snail, 220,
+           200,
+             40, 40);
+   apple(food.x, food.y);
+   //snakeBody(20, 20);
+   //apple(20,20);
    scoreText();
-
+   if(gameOver){
+     backCtx.fillStyle = 'Black';
+     backCtx.fillText("Game Over! Please Try Again", backBuffer.width/2 - 220, backBuffer.height/2);
+   }
 }
-
-
   //ctx.drawImage(backCanvas,0,0);
 /* Launch the game */
 window.requestAnimationFrame(loop);
